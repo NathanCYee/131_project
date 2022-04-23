80712786695d708343d6ee8842f9ba1d44fd2374
Nathan Yee <yee.nathan.c@gmail.com>
2022-04-23 01:19:09 -0700

    Updated README, begun work on login

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from app import db
-from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+"""class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
@@ -19,3 +20,20 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<User ID: {self.user_id} {self.body}>'
+"""
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User: {self.username} {self.email}>'
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 # E-Buy
+[![CircleCI](https://circleci.com/gh/NathanCYee/131_project/tree/master.svg?style=svg)](https://circleci.com/gh/NathanCYee/131_project/tree/master)
+[![codecov](https://codecov.io/gh/NathanCYee/131_project/branch/master/graph/badge.svg?token=G6YR4ZSL9J)](https://codecov.io/gh/NathanCYee/131_project)
 - Sarah Singh-LEAD (@SarahS16) 
 - Selim Ishakbeyoglu (@SelimIshakb)
 - Nicholas (@StickOnAStick)
