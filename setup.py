d70b76e0b9cef88ea7e52fb6df161b188ab6ae04
Nathan Yee <yee.nathan.c@gmail.com>
2022-04-23 00:31:26 -0700

    added pytest-cov requirement

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 tests_require = [
-    'pytest'
+    'pytest',
+    'pytest-cov'
 ]
 
 setup(
     name='e-buy',
     version='0.0.1',
     packages=['app'],
-    install_requires=['pytest', 'Flask', 'flask-wtf', 'flask-sqlalchemy', 'flask-login'],
+    install_requires=['pytest', 'pytest-cov', 'Flask', 'flask-wtf', 'flask-sqlalchemy', 'flask-login'],
     extras_require={
         'test': tests_require
     },
