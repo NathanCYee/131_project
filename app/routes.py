b9d7a01bd9fb87fa70e4e02964ac0c2a371ebf8a
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-04-28 20:19:32 -0700

    Added home page, updated base.html, added page switiching, added cart page, added search page. NEED TO DO: cart page, add functionality.

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,12 @@ from flask import render_template, flash
 
 @webapp.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
+
+@webapp.route('/search')
+def search():
+    return render_template('search.html')
+
+@webapp.route('/checkout')
+def cart():
+    return render_template('cart.html')
